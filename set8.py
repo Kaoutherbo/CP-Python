170ede0765df24bc7c9cd5d30098c35a090e5e3b
kaouther <kaoutherboutheldja@gmail.com>
2023-12-03 21:29:15 +0100

    Add first task in set 8

---
+++ set8.py
@@ -0,0 +1,26 @@
+# TASK 01 --------------------------------
+from math import sqrt
+
+def sec_deg_solver(a, b, c):
+    if a == 0:
+        if b != 0 and c != 0:
+            return f"It is a first degree equation. Solution: {round(-c / b, 10)}"
+        elif b == 0 and c == 0:
+            return "The equation is indeterminate"
+        elif b == 0 and c != 0:
+            return "Impossible situation. Wrong entries"
+        elif b != 0 and c == 0:
+            return f"It is a first degree equation. Solution: {round(0.0, 10)}"
+    else:
+        d = b ** 2 - 4 * a * c
+        if d < 0:
+            return "There are no real solutions"
+        elif d == 0:
+            return f"It has one double solution: {round(-b / (2 * a), 10)}"
+        else:
+            s1, s2 = (-b - sqrt(d)) / (2 * a), (-b + sqrt(d)) / (2 * a)
+            solutions = sorted([round(s1, 10), round(s2, 10)])
+            return f"Two solutions: {solutions[0]}, {solutions[1]}"
+
+# TASK 02 ------------------------------------------------------
+

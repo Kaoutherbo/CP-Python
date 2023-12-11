f138c4a4abc52e7b6c28b9605d287971581f3bfc
kaouther <kaoutherboutheldja@gmail.com>
2023-12-11 23:38:24 +0100

    Add set 12

--- set12.py
+++ set12.py
@@ -8,3 +8,14 @@ def something_acci(num_digits):
 
         if len(str(b)) >= num_digits:
             return position, len(str(b))
+
+
+def cumulative_triangle(n):
+    if n < 1 or n > 10000:
+        return "Invalid input. n should be between 1 and 10,000."
+
+    start_num = sum(range(1, n))
+
+    row_sum = sum(range(start_num + 1, start_num + n + 1))
+
+    return row_sum

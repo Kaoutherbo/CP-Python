4841d33fa8fbd04ec93919f84f1db2877b9892fc
kaouther <kaoutherboutheldja@gmail.com>
2024-01-17 22:13:33 +0100

    add set 16 challenge 1

---
+++ set16.py
@@ -0,0 +1,10 @@
+def wetermelon(w): 
+    if w %2 ==0:
+        num = w/2
+        if num % 2 == 0:
+            return "Yes"
+        else:
+            return "No"
+    else:
+        return "No"
+        

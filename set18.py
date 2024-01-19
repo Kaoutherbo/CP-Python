40c47f4c215c915eb87b92ed1075deb43853b866
kaouther <kaoutherboutheldja@gmail.com>
2024-01-19 19:14:07 +0100

    add set

--- set18.py
+++ set18.py
@@ -49,7 +49,15 @@ def wordCapitalize(word):
 
 
 """ 
-    
+    Input:
+    The first line contains integer n (1 ≤ n ≤ 50) — the number of stones on the table.
+    The next line contains string s, which represents the colors of the stones. 
+    We'll consider the stones in the row numbered from 1 to n from left to right. 
+    Then the i-th character s equals "R", if the i-th stone is red, "G", 
+    if it's green and "B", if it's blue.
+
+    Output:
+    Print a single integer — the answer to the problem.
 """
 
 def stone(n, statement):

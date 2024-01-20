79475afb8307676a8ec5f37e1bedae15e64e4ba8
kaouther <kaoutherboutheldja@gmail.com>
2024-01-20 22:44:21 +0100

    add set 19 challenge 2

--- set19.py
+++ set19.py
@@ -6,3 +6,20 @@ def oddDivisor(n):
             return "YES"
     return "NO"
 
+"""
+    sale: Function that return the maximum sum of money that 
+    Bob can earn, given that he can carry at most m TV sets.
+"""
+
+def sale(nums, m):
+    nums.sort()  
+    money = 0
+
+    for num in nums:
+        if num <= 0 and m > 0:
+            money += abs(num)  
+            m -= 1
+
+    return money
+
+
